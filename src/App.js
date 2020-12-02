fece0fd54588f212e754879dc7333869490397cb
PeterOsoo <posoo617@gmail.com>
2020-12-02 19:23:13 +0300

    GlobalStyles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import "./App.css"
 
 import Navbar from "./components/Navbar/Navbar"
+import { GlobalStyle } from "./globalStyles"
 
 const App = () => {
 	return (
 		<>
+			<GlobalStyle />
 			<Navbar />
 		</>
 	)
