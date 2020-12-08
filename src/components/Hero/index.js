0bcb93a2df8dd59ba1760737335636ba300f06fb
PeterOsoo <posoo617@gmail.com>
2020-12-08 15:40:06 +0300

    Hero Section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 import Navbar from "../Navbar/Navbar"
-import { HeroContainer } from "./HeroElements"
+
+import {
+	HeroContainer,
+	HeroContent,
+	HeroItems,
+	HeroH1,
+	HeroP,
+	HeroBtn,
+} from "./HeroElements"
 
 const Hero = () => {
 	return (
 		<div>
 			<HeroContainer>
 				<Navbar />
+				<HeroContent>
+					<HeroItems>
+						<HeroH1>Greatest Pizza Ever</HeroH1>
+						<HeroP>Ready in 60 seconds</HeroP>
+						<HeroBtn>Place Order</HeroBtn>
+					</HeroItems>
+				</HeroContent>
 			</HeroContainer>
 		</div>
 	)
