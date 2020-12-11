4b0c7c07014081041275b323b5257a0038753ada
PeterOsoo <posoo617@gmail.com>
2020-12-11 20:57:41 +0300

    add products data

---
+++ src/components/Products/data.js
@@ -0,0 +1,33 @@
+import product1 from "../../images/product-1.jpg"
+import product2 from "../../images/product-2.jpg"
+import product3 from "../../images/product-3.jpg"
+
+export const productData = [
+	{
+		img: product1,
+		alt: "Pizza",
+		name: "Cheese Pizza",
+		desc:
+			"Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+		price: "Kshs 1999",
+		button: "Add to Cart",
+	},
+	{
+		img: product2,
+		alt: "Pizza",
+		name: "Margherita Pizza",
+		desc:
+			" Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+		price: "Kshs 1699",
+		button: "Add to Cart",
+	},
+	{
+		img: product3,
+		alt: "Pizza",
+		name: "BBQ Chicken Pizza",
+		desc:
+			" Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+		price: "Kshs 1499",
+		button: "Add to Cart",
+	},
+]
